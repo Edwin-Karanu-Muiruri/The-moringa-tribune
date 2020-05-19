ac168f2f5e8bf401e5f3dde606ef781650bab26f
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-05-19 16:58:48 +0300

    Adding dynamic content,adding a dynamic url, and creating the first template.

--- news/views.py
+++ news/views.py
@@ -1,6 +1,47 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
+import datetime as dt
 
 #our views functions
 def welcome(request):
     return HttpResponse('Welcome to The Moringa Tribune')
+
+def convert_dates(dates):
+    #This function should get the weekday number for the date
+    day_number = dt.date.weekday(dates)
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+
+    day = days[day_number]
+    return day
+
+def news_of_day(request):
+    date = dt.date.today()
+    
+    #function to convert date object to find exact day
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1> News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+
+def past_days_news(request,past_date):
+    #Convert data from the string url
+    try:
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        raise Http404()
+
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
